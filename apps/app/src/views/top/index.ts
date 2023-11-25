681348694ad72fbefbd49f61cdfb29993d01ef91
jack <jack@github.com>
2023-11-25 11:41:41 +0800

    change home page

--- apps/app/src/views/top/index.ts
+++ apps/app/src/views/top/index.ts
@@ -1,5 +1,6 @@
 import { TailwindElement, html, customElement } from '@riffian-web/ui/src/shared/TailwindElement'
 import '~/components/top/list'
+import '~/components/createAlbum/btn'
 
 // Style
 import style from './index.css?inline'
@@ -8,8 +9,13 @@ import style from './index.css?inline'
 export class ViewTop extends TailwindElement(style) {
   render() {
     return html`<div class="top">
-      <div class="ui-container">
-        <p class="font-bold text-xl">Top Music</p>
+      <div class="ui-container relative flex justify-between items-center">
+        <div class="flex items-center gap-3 lg_gap-4 lg_w-40">
+          <p class="font-bold text-xl">Weekly</p>
+        </div>
+        <div class="flex justify-end items-center lg_w-40">
+          <create-album-btn></create-album-btn>
+        </div>
       </div>
       <top-album></top-album>
     </div>`
