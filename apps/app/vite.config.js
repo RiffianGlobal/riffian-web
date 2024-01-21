08babbdab04573d00642df404c2598bf682e7d25
Issac <Issac>
2024-01-22 01:07:22 +0800

    chg: disable pwa temporarily

--- apps/app/vite.config.js
+++ apps/app/vite.config.js
@@ -1,10 +1,27 @@
+import { env } from '@lit-web3/base/vite.config.base'
 import viteAppConfig from '@lit-web3/base/vite.config.app'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default ({ mode = '' }) => {
   return viteAppConfig({
     server: { port: 3000 },
     build: {
       emptyOutDir: mode === 'development'
-    }
+    },
+    // S TODO: remove this after app@^1.0.0
+    viteAppConfig: { pwa: false },
+    plugins: [
+      VitePWA({
+        selfDestroying: true,
+        registerType: 'autoUpdate',
+        manifest: {
+          name: env.VITE_APP_TITLE || env.npm_package_displayName,
+          short_name: env.VITE_APP_NAME,
+          lang: 'en',
+          background_color: env.VITE_APP_BG
+        }
+      })
+    ]
+    // E
   })({ mode })
 }
