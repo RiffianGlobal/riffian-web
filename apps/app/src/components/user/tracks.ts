6692598732e1c6612a3a4be8150162e744aff3ec
Gadiel <>
2024-02-01 20:13:03 +0800

    wip: responsive ui

--- apps/app/src/components/user/tracks.ts
+++ apps/app/src/components/user/tracks.ts
@@ -9,7 +9,10 @@ import {
   when
 } from '@riffian-web/ui/shared/theme-element'
 import { bridgeStore, StateController } from '@riffian-web/ethers/src/useBridge'
+import { screenStore } from '@lit-web3/base/screen'
 import { format } from '~/lib/dayjs'
+import { asyncReplace } from 'lit/directives/async-replace.js'
+import { timeAgo } from '~/lib/dayjs'
 
 import { tracks } from './action'
 import { formatUnits } from 'ethers'
@@ -27,12 +30,17 @@ import style from './tracks.css?inline'
 @customElement('track-list')
 export class TrackInfo extends ThemeElement(style) {
   bindBridge: any = new StateController(this, bridgeStore)
+  bindScreen: any = new StateController(this, screenStore)
   @property({ type: Boolean }) weekly = false
   @property({ type: String }) address = ''
   @state() trackList: any = []
   @state() pending = false
   @state() err = ''
   @state() ts = 0
+
+  get isMobi() {
+    return screenStore.screen.isMobi
+  }
 
   get disabled() {
     return !bridgeStore.bridge.account
@@ -44,6 +52,35 @@ export class TrackInfo extends ThemeElement(style) {
     } else {
       goto(`/track/${item.address}`)
     }
+  }
+
+  timeAgo = async function* (timestamp: number | string) {
+    while (true) {
+      yield timeAgo(timestamp)
+      await new Promise((r) => setTimeout(r, 1000))
+    }
+  }
+
+  itemMobi = (item: any) => {
+    return html`<div class="w-full overflow-hidden flex gap-x-2">
+      <div class="w-[3.25rem] h-[3.25rem] mr-2 rounded-lg">
+        <img-loader src=${item.image} class="w-[3.25rem] rounded-lg"></img-loader>
+      </div>
+      <div class="flex-auto flex flex-col">
+        <div class="flex truncate items-center">
+          <p class="truncate">${item.name}</p>
+          <a href=${item.uri} class="flex-none ml-1.5" target="_blank"
+            ><i class="text-lg mdi mdi-play-circle-outline"></i
+          ></a>
+        </div>
+        <!-- other info -->
+        <div class="mt-0.5 text-xs text-gray-300/60 opacity-80">${asyncReplace(this.timeAgo(item.createdAt))}</div>
+      </div>
+      <div class="w-16 flex-none justify-center text-right">
+        <div class="text-base">${item.totalVal}</div>
+        <div class="text-xs opacity-80">${item.supply}<span class="text-gray-300/60 ml-1">tickets</span></div>
+      </div>
+    </div>`
   }
 
   init = async () => {
@@ -71,15 +108,23 @@ export class TrackInfo extends ThemeElement(style) {
   }
 
   render() {
-    return html`<div role="list" class="ui-list py-6 ${classMap(this.$c([this.pending ? 'loading' : 'hover']))}">
-      <div class="flex header">
-        <div class="w-16">Index</div>
-        <div class="flex-auto">Name</div>
-        <div class="flex-none w-40">Created</div>
-        <div class="flex-none w-20 text-right">Voters</div>
-        <div class="flex-none w-24 text-right">Tickets</div>
-        <div class="flex-none w-24 text-right">Vote Value</div>
-      </div>
+    return html`<div
+      role="list"
+      class="ui-list py-4 md_py-6 ${classMap(this.$c([this.pending ? 'loading' : 'hover']))}"
+    >
+      ${when(
+        !this.isMobi,
+        () => html`
+          <div class="flex header">
+            <div class="flex-none w-16">Index</div>
+            <div class="flex-auto">Name</div>
+            <div class="flex-none w-40">Created</div>
+            <div class="num flex-none">Voters</div>
+            <div class="num flex-none">Tickets</div>
+            <div class="num flex-none">Vote Value</div>
+          </div>
+        `
+      )}
       ${when(
         !this.ts && !this.trackList.length,
         () =>
@@ -95,30 +140,30 @@ export class TrackInfo extends ThemeElement(style) {
           ${repeat(
             this.trackList,
             (item: any, i) =>
-              html`<div class="item flex py-2 pr-2 items-center cursor-pointer " @click=${() => this.go2(item)}>
-                <div class="flex-none w-16 pl-4 text-sm font-light opacity-75">
-                  ${i + 1}
-                  ${when(this.trackList.length > 3 && i < 3, () => html`<i class="mdi mdi-fire text-red-400"></i>`)}
-                </div>
-                <div class="flex-auto flex">
-                  <div class="w-[3.25rem] h-[3.25rem] mr-4 rounded-lg">
-                    <img-loader src=${item.image} class="rounded-lg"></img-loader>
-                  </div>
-                  <div>
-                    <p class="name truncate">${item.name}</p>
-                    <span class="icon mt-1"><i class="mdi mdi-play-circle-outline"></i></span>
-                  </div>
-                </div>
-                <div class="flex-none w-40 text-xs text-gray-300/60">${format(item.createdAt)}</div>
-                <div class="flex-none w-20 text-right text-sm">
-                  <p class="num truncate mt-2">${item.fansNumber}</p>
-                </div>
-                <div class="flex-none w-24 text-right text-sm">
-                  <p class="num truncate mt-2">${item.supply}</p>
-                </div>
-                <div class="flex-none w-24 text-right text-sm">
-                  <p class="num truncate mt-2">${item.totalVal}</p>
-                </div>
+              html`<div class="item flex py-2 pr-2 items-center cursor-pointer" @click=${() => this.go2(item)}>
+                ${when(
+                  !this.isMobi,
+                  () => html`
+                    <div class="flex-none w-16 pl-4 text-sm font-light opacity-75">
+                      ${i + 1}
+                      ${when(this.trackList.length > 3 && i < 3, () => html`<i class="mdi mdi-fire text-red-400"></i>`)}
+                    </div>
+                    <div class="flex-auto flex">
+                      <div class="w-[3.25rem] h-[3.25rem] mr-4 rounded-lg">
+                        <img-loader src=${item.image} class="rounded-lg"></img-loader>
+                      </div>
+                      <div>
+                        <p class="name truncate">${item.name}</p>
+                        <span class="icon mt-1"><i class="mdi mdi-play-circle-outline"></i></span>
+                      </div>
+                    </div>
+                    <div class="flex-none md_w-40 text-xs text-gray-300/60">${format(item.createdAt)}</div>
+                    <div class="num text-sm truncate">${item.fansNumber}</div>
+                    <div class="num text-sm truncate">${item.supply}</div>
+                    <div class="num text-sm ">${item.totalVal}</div>
+                  `,
+                  () => html`${this.itemMobi(item)}`
+                )}
               </div>`
           )}
         `
--- apps/app/src/views/user/index.ts
+++ apps/app/src/views/user/index.ts
@@ -10,27 +10,15 @@ export class TrackPage extends ThemeElement(style) {
   @property({ type: String }) addr = ''
 
   render() {
-    return html`<div class="ui-container mx-auto flex flex-col place-content-center pt-10">
+    return html`<div class="ui-container mx-auto flex flex-col place-content-center md_pt-10">
       <!-- user profile -->
-      <user-detail .address=${this.addr} class="mx-auto"></user-detail>
+      <user-detail .address=${this.addr} class="md_mx-auto"></user-detail>
 
+      <!-- created -->
       <div class="w-full inline-flex pb-2 border-b border-slate-50/10">
         <div class="py-1.5 px-3 text-base font-normal text-white/70 rounded-md">Tracks</div>
       </div>
-
       <track-list .address=${this.addr}></track-list>
-
-      <!-- <div class="h-20 pt-1 mb-8">
-        <div class="font-bold text-xl">
-          <user-detail .address=${this.addr}> </user-detail>
-        </div>
-      </div>
-      <div class="h-10 pt-1 mt-20">
-        <div class="font-bold text-xl">Tracks</div>
-      </div>
-      <div class="mt-0">
-        <track-list .address=${this.addr}></track-list>
-      </div> -->
     </div>`
   }
 }
