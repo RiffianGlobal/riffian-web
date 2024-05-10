8b097d9dddc7c63e991ad6f193d8c8b1c9cbbdef
laotoumaizi <laotoumaizi@gmail.com>
2024-05-10 22:30:39 +0800

    web rookie tips layout complete

--- packages/ui/src/tip/rookie.ts
+++ packages/ui/src/tip/rookie.ts
@@ -7,13 +7,11 @@ import style from './rookie.css?inline'
 @customElement('ui-tip-rookie')
 export class UITipRookie extends ThemeElement(style) {
 
-  @state() show = true
-
   @property({type: Boolean})
-  open = false;
+  show = false;
 
   close() {
-    this.open = false;
+    this.show = false;
   }
 
   render() {
@@ -21,20 +19,37 @@ export class UITipRookie extends ThemeElement(style) {
       ${when(
         screenStore.isMobi,
         () => html`
-          <div>
+          <div class="backdrop relative">
             <p>mobile rookie tips</p>
           </div>
         `,
         () => html`
-          <div class="backdrop">
+          <div class="backdrop relative">
             <div class="modal">
-              <button class="close" @click="${this.close}">X</button>
+              <button class="close" @click="${this.close}">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  class="w-6 h-6">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M6 18L18 6M6 6l12 12"/>
+                </svg>
+              </button>
               <p class="font-semibold text-xl mb-4 text-center">Come to RIFFIAN and earn $DOID!</p>
               <p class="content-text">You can claim rewards and get $DOID; </p>
               <p class="content-text">can vote for the content you like, and as long as it enters the weekly list, you can also get a share of the prize pool rewards; </p> 
               <p class="content-text">You can vote for the content you like at a low price, then wait for others to vote and then sell it to get the price difference.</p>
               <p class="content-text font-light">Come and play together!</p>
-              <button class="play-button">Let's Play</button>
+              <button class="play-button" @click="${this.close}">Let's Play</button>
+              <div class="top-left-logo"></div>
+              <div class="left-top-logo"></div>
+              <div class="left-bottom-logo"></div>
+              <div class="right-bottom-logo"></div>
+              <div class="right-bottom-free-logo"></div>
             </div>
           </div>
         `
