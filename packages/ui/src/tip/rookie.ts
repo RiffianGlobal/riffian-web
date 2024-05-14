e35d75147468ce535306b7863d6370ca7715e71a
laotoumaizi <laotoumaizi@gmail.com>
2024-05-15 00:56:39 +0800

    complate mobile rookie tips

--- packages/ui/src/tip/rookie.ts
+++ packages/ui/src/tip/rookie.ts
@@ -35,11 +35,17 @@ export class UITipRookie extends ThemeElement(style) {
                       d="M6 18L18 6M6 6l12 12"/>
                   </svg>
                 </button>
-                <p class="font-medium text-sm mt-6 mb-4 text-center">Come to RIFFIAN and earn $DOID!</p>
+                <p class="font-medium text-sm mt-8 mb-4 text-center">Come to RIFFIAN and earn $DOID!</p>
                 <p class="content-text-mobile">You can claim rewards and get $DOID; </p>
                 <p class="content-text-mobile">can vote for the content you like, and as long as it enters the weekly list, you can also get a share of the prize pool rewards; </p> 
                 <p class="content-text-mobile">You can vote for the content you like at a low price, then wait for others to vote and then sell it to get the price difference.</p>
-                <p class="content-text-mobile font-normal">Come and play together!</p>
+                <p class="font-bold text-xs mt-4 ml-6 mr-4 text-left  ">Come and play together!</p>
+                <button class="play-button-mobile" @click="${this.close}">Let's Play</button>
+                <div class="top-left-mobile-logo"></div>
+                <div class="left-center-mobile-logo"></div>
+                <div class="left-bottom-mobile-logo"></div>
+                <div class="right-bottom-mobile-logo"></div>
+                <div class="right-bottom-free-mobile-logo"></div>
               </div>
             </div>
           </div>
